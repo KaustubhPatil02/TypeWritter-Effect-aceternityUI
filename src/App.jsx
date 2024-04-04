7bb004ba4a0fd55597a92d85533c577f560bfe2c
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-04-04 23:55:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TypewriterEffectSmoothDemo } from './TypewriterEffectSmoothDemo'
+
+const App = () => {
+  return (
+    <TypewriterEffectSmoothDemo/>
+  )
+}
+
+export default App
